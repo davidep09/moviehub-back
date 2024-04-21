330c5f1202f4fd8f09698f32e9a70a99efa044e9
David <davidep@alumnos.iesgalileo.es>
2024-04-21 20:38:44 +0200

    First commit

---
+++ src/main/java/com/moviehub/mhback/MhBackApplication.java
@@ -0,0 +1,13 @@
+package com.moviehub.mhback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moviehub/mhback/MhBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviehub.mhback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
